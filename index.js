9b0cc9c0fcd89e7886453ffe2c7fb76b99bebbff
DESKTOP-TA2I9Q2\limso <limson_95@naver.com>
2019-05-18 20:33:16 +0900

    START server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 7003
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
+  
+  app.get('/',(req,res)=> res.send("hello world"))
+  
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500)
+      res.render('error', {
+        message: err.message,
+        error: err
+      })
+    })
+  }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err)
+  })
+
+app.listen(port,() => console.log('stuConcilServer is running'))
