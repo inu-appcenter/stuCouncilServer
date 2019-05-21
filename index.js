38657aa1bad0a5088739a88d10e6b0a46f77ac9e
임동완 <limson_95@naver.com>
2019-05-21 14:03:24 +0900

    ADD cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 const port = 7003
@@ -9,6 +10,7 @@ const login = require('./routes/login')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/board',board)
 app.use('/login',login)
