684ab40b1b4a19457316ce6dff0475368825dc0e
임동완 <limson_95@naver.com>
2019-05-24 22:36:37 +0900

    FIX update fileArray

--- routes/board.js
+++ routes/board.js
@@ -103,6 +103,7 @@ router.post('/create',upload.array('userFile',4),async (req,res) => {
 })
 
 router.post('/update',upload.array('userFile',4),async (req,res) => {
+    fileArray = []
     await req.files.map(Data => fileArray.push(Data.filename))
     let updateQuery = {
         boardId : req.body.boardId,
