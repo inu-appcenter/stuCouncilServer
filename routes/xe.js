4d7f92a847d2e1601052d27bc95d203927db1db3
임동완 <limson_95@naver.com>
2019-09-30 17:32:13 +0900

    xe original URL

--- routes/xe.js
+++ routes/xe.js
@@ -2,11 +2,13 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/:where/:boardId',(req,res)=>{
+    console.log(req.originalUrl)
     res.redirect("http://withyou27.byus.net/xe/"+req.params.where+"/"+req.params.boardId)
 })
 
 router.get('/',(req,res)=>{
-    res.redirect("http://withyou27.byus.net/xe/?module=file&act=procFileDownload&file_srl="+req.query.file_srl+"&sid="+req.query.sid)
+    res.redirect("http://withyou27.byus.net"+req.originalUrl)
+    // res.redirect("http://withyou27.byus.net/xe/?module=file&act=procFileDownload&file_srl="+req.query.file_srl+"&sid="+req.query.sid)
     // http://we.incheon.ac.kr/xe/?module=file&act=procFileDownload&file_srl=5734589&sid=9a28a0b12ce3cabc8c4dba294fd94018
     // http://we.incheon.ac.kr/xe/?module=file&act=procFileDownload&file_srl=5733888&sid=ddd584784eee9ff98080edcec46aaf5d
 })
